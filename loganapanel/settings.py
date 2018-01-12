7c0a4fc88fadc21f222b07cab78798cad4e1b925
Bernardo <bernactkj@gmail.com>
2018-01-12 03:40:17 -0200

    Commit

--- loganapanel/settings.py
+++ loganapanel/settings.py
@@ -42,8 +42,6 @@ INSTALLED_APPS = [
     'loganapanel.forum_logs'
 ]
 
-print("PRINT-------------" + os.path.join(BASE_DIR, 'loganapanel/templates'))
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -112,4 +110,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
